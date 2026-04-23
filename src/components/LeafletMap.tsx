66494514ed44e0db945998e1da5540d49126cb2e
Luxe Admin <admin@luxeliving.do>
2026-04-23 15:12:56 -0400

    Mapa: selector de estilo en inferior derecha (mobile encima de la barra de navegacion)

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -118,7 +118,7 @@ export default function LeafletMap({
 
   return (
     <div className="relative h-full w-full">
-      <div className="absolute top-3 right-3 z-[402]">
+      <div className="absolute right-3 bottom-24 lg:bottom-3 z-[402]">
         <MapStyleSwitcher value={styleKey} onChange={setStyleKey} />
       </div>
     <MapContainer center={CENTER} zoom={14} style={{ height: "100%", width: "100%" }} zoomControl={false}>
